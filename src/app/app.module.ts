099e8d62046835944f7dce5f59cfdb8d34925a78
tahaalp6 <tahaalp1998@hotmail.com>
2020-08-06 14:31:52 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignupComponent } from './signup/signup.component';
+import { FormComponent } from './components/form/form.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    FormComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
